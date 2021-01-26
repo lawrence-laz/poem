bb393d878cab740444aff0ffb473c8209477a697
Laurynas Lazauskas <laurynas@lazauskas.info>
2021-01-26 20:47:52 +0200

    Add ToBase64String extension method

---
+++ src/Poem/Extensions/PoemConvertExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace System
+{
+    /// <summary>
+    /// Extension methods for fluent handling of <see cref="Convert"/> operations.
+    /// </summary>
+    public static class PoemConvertExtensions
+    {
+        /// <inheritdoc cref="Convert.ToBase64String(byte[])"/>
+        public static string ToBase64String(this byte[] inArray) => Convert.ToBase64String(inArray);
+    }
+}
---
+++ test/Poem.Tests/Extensions/PoemConvertExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Poem.Tests.Extensions
+{
+    public class PoemConvertExtensionsTests
+    {
+        [Theory, AutoData]
+        public void Calling_ToBase64String_returns_same_result_as_standard_method(byte[] bytes)
+        {
+            // Arrange
+            var expected = Convert.ToBase64String(bytes);
+
+            // Act
+            var actual = bytes.ToBase64String();
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+    }
+}
