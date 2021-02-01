27884f3cc076d7d87cbedec6c481b5b5e268d223
Laurynas Lazauskas <laurynas@lazauskas.info>
2021-02-01 22:57:32 +0200

    Add PoemListExtensions with AddToStart

---
+++ test/Poem.Tests/Extensions/Collections/Generic/PoemListExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Poem.Tests.Extensions.Collections.Generic
+{
+    public class PoemListExtensionsTests
+    {
+        [Theory, AutoData]
+        public void Calling_AddToStart_on_null_should_add_element_to_start(IEnumerable<object> expected)
+        {
+            // Arrange
+            var actual = new List<object>(expected.Skip(1));
+
+            // Act
+            actual.AddToStart(expected.First());
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Theory, AutoData]
+        public void Calling_AddToStart_on_null_should_throw(object anyItem)
+        {
+            // Arrange
+            List<object> actual = null;
+
+            // Act & Assert
+            actual.Invoking(x => x.AddToStart(anyItem))
+                .Should().Throw<NullReferenceException>();
+        }
+    }
+}
