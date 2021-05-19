05403d4554f3870ae25b999284ce15bd5964e7f7
Laurynas Lazauskas <laurynas@lazauskas.info>
2021-05-19 16:01:42 +0300

    Add `ParseEnum` methods

---
+++ src/Poem/Extensions/PoemEnumExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace System
+{
+    /// <summary>
+    /// Extension methods for fluent handling of <see cref="Enum"/> objects.
+    /// </summary>
+    public static class PoemEnumExtensions
+    {
+        /// <inheritdoc cref="Enum.Parse(Type, string)"/>
+        public static object ParseEnum(this string value, Type enumType)
+        {
+            return Enum.Parse(enumType, value);
+        }
+
+        /// <summary>
+        /// Converts the string representation of the name or numeric value of one or more
+        /// enumerated constants to an equivalent enumerated object.
+        /// </summary>
+        /// <typeparam name="T">The enumeration type to which to convert value.</typeparam>
+        /// <param name="value">A string containing the name or value to convert.</param>
+        /// <returns>An object of specified type whose value is represented by value.</returns>
+        public static T ParseEnum<T>(this string value)
+        {
+            return (T)Enum.Parse(typeof(T), value);
+        }
+    }
+}
---
+++ test/Poem.Tests/Extensions/PoemEnumExtensions.cs
@@ -0,0 +1,44 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Poem.Tests.Extensions
+{
+    public class PoemEnumExtensions
+    {
+        public enum TestEnum
+        {
+            Foo,
+            Bar
+        }
+
+        [Theory, AutoData]
+        public void Calling_parse_enum_with_string_returns_same_result_as_standard_method(TestEnum expected)
+        {
+            // Arrange
+            var enumString = expected.ToString();
+
+            // Act
+            var actual = enumString.ParseEnum(typeof(TestEnum));
+
+            // Assert
+            actual.Should().Be(expected);
+            actual.Should().Be(Enum.Parse(typeof(TestEnum), enumString));
+        }
+
+        [Theory, AutoData]
+        public void Calling_parse_enum_with_type_and_string_returns_same_result_as_standard_method(TestEnum expected)
+        {
+            // Arrange
+            var enumString = expected.ToString();
+
+            // Act
+            var actual = enumString.ParseEnum<TestEnum>();
+
+            // Assert
+            actual.Should().Be(expected);
+            actual.Should().Be(Enum.Parse(typeof(TestEnum), enumString));
+        }
+    }
+}
